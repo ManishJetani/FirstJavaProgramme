c038bf8ac07ff8adedfe11d1a125880d592b5355
ManishJetani <manishjetani14@gmail.com>
2021-03-14 21:42:01 +0000

    homework of intelij

---
+++ src/Calculator1.java
@@ -0,0 +1,20 @@
+public class Calculator1 {
+    static int a = 40;
+    static int b = 10;
+    static int total = (a+b);
+    static int minus = (a-b);
+    static int multiplication = (a*b);
+    static int division = (a/b);
+
+    public static void main(String[] args) {
+
+
+      System.out.println("Addition of "+ a +" & "+ b +" = "+total);
+      System.out.println("Subtraction of " + a + " - "+ b +" = "+minus );
+      System.out.println("Multiplication of "+ a + " & "+ b + " = "+multiplication);
+      System.out.println("Division of "+ a + " / "+ b + " = "+division);
+
+
+    }
+
+}
---
+++ src/PersonalDetails.java
@@ -0,0 +1,35 @@
+public class PersonalDetails {
+//    static String name = "Boris";
+//    static String add = " 10, London Road,";
+//    static String postCode = " SW10 5AA";
+//    static String mobile = " 07891234567";
+    static int age = 55;
+    static Double security = 35.60;
+//    boolean vote = 55 > 18;
+//    char grade = 'b';
+
+    public static void main(String[] args) {
+        String name = "Boris";
+        String surname = " Johnson";
+        String add = "10,London Road,";
+        String postCode = "SW10 5AA";
+        String mobile = "07891234567";
+        boolean vote = age>18;
+        char grade = 'b';
+
+        System.out.println("Name : " + name + surname);
+        System.out.println("Address : "+ add + postCode );
+        System.out.println("Mobile No.: "+ mobile);
+        System.out.println("Age : "+ age );
+        System.out.println("Security Code : "+ security);
+        System.out.println("Eligible for vote = "+ vote);
+        System.out.println("Grade = "+ grade);
+
+
+    }
+
+
+
+
+
+}
